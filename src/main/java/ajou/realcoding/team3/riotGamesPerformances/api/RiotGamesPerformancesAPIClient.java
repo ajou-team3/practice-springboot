ad99c5d4d7d6fe79926dc5ee307bc291a7261839
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-14 22:41:47 +0900

    create api package and add RiotGamesPerformancesAPIClient class

---
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
@@ -0,0 +1,24 @@
+package ajou.realcoding.team3.riotGamesPerformances.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RiotGamesPerformancesAPIClient {
+
+    /*
+    private final String apikey
+    private final String summonerV4URL
+    private final String leagueV4Url
+    */
+
+    @Autowired
+    RestTemplate resttemplate;
+
+ /*
+    public PlayerEncryptedID requestEncryptedID(String summonerName)
+    public PlayerPerformance requestPerformance(String encryptedSummonerId)
+ */
+
+}
