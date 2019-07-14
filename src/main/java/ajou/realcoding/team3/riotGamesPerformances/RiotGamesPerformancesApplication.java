2dd768dd42eb08abb1bb7404799380a6760325bd
Joylish <yeonzzu45@ajou.ac.kr>
2019-07-14 20:10:15 +0900

    create springboot project using spring initialzr

---
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/RiotGamesPerformancesApplication.java
@@ -0,0 +1,13 @@
+package ajou.realcoding.team3.riotGamesPerformances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiotGamesPerformancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiotGamesPerformancesApplication.class, args);
+	}
+
+}
