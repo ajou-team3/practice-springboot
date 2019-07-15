0efeed8f51a67968cf6f29c0fc9d56fd3812b5dd
dddff22 <dddfff22@gmail.com>
2019-07-15 18:59:40 +0900

    change playperformace returnvalut to list

--- src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
@@ -1,23 +1,30 @@
 package ajou.realcoding.team3.riotGamesPerformances.api;
 
+import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerEncryptedID;
+import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerPerformance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RiotGamesPerformancesAPIClient {
 
-    private final String apikey = "RGAPI-ec1bf40e-0377-4c43-a4c4-92cc97032e15";
+    private final String apikey = "RGAPI-cc7b9a73-1649-482d-b1ef-4d73056f7042";
     private final String summonorV4Url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={apikey}";
     private final String leagueV4Url = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={apikey}";
 
 
     @Autowired
-    RestTemplate resttemplate;
+    RestTemplate restTemplate;
 
- /*
-    public PlayerEncryptedID requestEncryptedID(String summonerName)
-    public PlayerPerformance requestPerformance(String encryptedSummonerId)
- */
+    public PlayerEncryptedID requestEncryptedID(String summonerName) {
+        return restTemplate.exchange(summonorV4Url, HttpMethod.GET, null, PlayerEncryptedID.class, summonerName, apikey).getBody();
+    }
+
+    public PlayerPerformance[] requestPerformance(String encryptedSummonerId) {
+        PlayerPerformance[] performances = restTemplate.exchange(leagueV4Url, HttpMethod.GET, null, PlayerPerformance[].class, encryptedSummonerId, apikey).getBody();
+        return performances;
+    }
 
 }
