e10f12dd72ac8db36a136f3d8b4bd8b3a80452d4
YongJae <sdc689@gmail.com>
2019-07-15 19:45:55 +0900

    Add repository class - CurrentPerformanceRepository

---
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/repository/CurrentPerformanceRepository.java
@@ -0,0 +1,23 @@
+package ajou.realcoding.team3.riotGamesPerformances.repository;
+
+import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerPerformance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CurrentPerformanceRepository {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void manageCurrentPerformance(PlayerPerformance playerPerformance){
+        Query query = Query.query(Criteria.where("summonerId").is(playerPerformance.getSummonerId()));
+
+        mongoTemplate.insert(playerPerformance);
+
+    }
+}
