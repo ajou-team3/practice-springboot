f1c1d679dfc5f8bd26137798e9aa4315e12d7830
dddff22 <dddfff22@gmail.com>
2019-07-15 18:04:38 +0900

    check dependency

--- src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
@@ -7,11 +7,10 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class RiotGamesPerformancesAPIClient {
 
-    /*
-    private final String apikey
-    private final String summonerV4URL
-    private final String leagueV4Url
-    */
+    private final String apikey = "RGAPI-ec1bf40e-0377-4c43-a4c4-92cc97032e15";
+    private final String summonorV4Url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={apikey}";
+    private final String leagueV4Url = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={apikey}";
+
 
     @Autowired
     RestTemplate resttemplate;
