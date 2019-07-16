01c8f43fd6bc3bc35421c015172cb62962e8d581
YongJae <sdc689@gmail.com>
2019-07-16 22:45:33 +0900

    Add service - getPerformanceByEncryptedSummonerID method

--- src/main/java/ajou/realcoding/team3/riotGamesPerformances/service/GameService.java
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/service/GameService.java
@@ -3,19 +3,25 @@ package ajou.realcoding.team3.riotGamesPerformances.service;
 import ajou.realcoding.team3.riotGamesPerformances.api.RiotGamesPerformancesAPIClient;
 import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerEncryptedID;
 import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerPerformance;
+import ajou.realcoding.team3.riotGamesPerformances.repository.CurrentPerformanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameService {
     @Autowired
     RiotGamesPerformancesAPIClient riotGamesPerformancesAPIClient;
+    @Autowired
+    CurrentPerformanceRepository currentPerformanceRepository;
 
     public PlayerEncryptedID getEncryptedIdBySummonerName(String summonerName) {
         return riotGamesPerformancesAPIClient.requestEncryptedID(summonerName);
     }
 
-    public PlayerPerformance getPerformanceByEncryptedSummonerID(String encryptedID) {
-        return null;
+    public List<PlayerPerformance> getPerformanceByEncryptedSummonerID(String encryptedID) {
+        currentPerformanceRepository.manageCurrentPerformance(riotGamesPerformancesAPIClient.requestPerformance(encryptedID));
+        return currentPerformanceRepository.findCurrentPerformance(encryptedID);
     }
 }
