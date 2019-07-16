db857a2c90c30b6f662d850a6ff48c8009763908
0zoo <e0zoo94@gmail.com>
2019-07-16 12:23:45 +0900

    added logic to get encryptedId found by summoner name from riot games server

--- src/main/java/ajou/realcoding/team3/riotGamesPerformances/controller/PerformanceController.java
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/controller/PerformanceController.java
@@ -1,4 +1,21 @@
 package ajou.realcoding.team3.riotGamesPerformances.controller;
 
+import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerPerformance;
+import ajou.realcoding.team3.riotGamesPerformances.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PerformanceController {
+    @Autowired
+    GameService gameService;
+
+    @GetMapping("/lol/summoner/get-player-performance/by-name/{summonerName}")
+    public PlayerPerformance getPlayerPerformance(@PathVariable String summonerName){
+        String encryptedID = gameService.getEncryptedIdBySummonerName(summonerName).getId();
+        return gameService.getPerformanceByEncryptedSummonerID(encryptedID);
+    }
+
 }
--- src/main/java/ajou/realcoding/team3/riotGamesPerformances/service/GameService.java
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/service/GameService.java
@@ -1,4 +1,21 @@
 package ajou.realcoding.team3.riotGamesPerformances.service;
 
+import ajou.realcoding.team3.riotGamesPerformances.api.RiotGamesPerformancesAPIClient;
+import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerEncryptedID;
+import ajou.realcoding.team3.riotGamesPerformances.domain.PlayerPerformance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
+    @Autowired
+    RiotGamesPerformancesAPIClient riotGamesPerformancesAPIClient;
+
+    public PlayerEncryptedID getEncryptedIdBySummonerName(String summonerName) {
+        return riotGamesPerformancesAPIClient.requestEncryptedID(summonerName);
+    }
+
+    public PlayerPerformance getPerformanceByEncryptedSummonerID(String encryptedID) {
+        return null;
+    }
 }
