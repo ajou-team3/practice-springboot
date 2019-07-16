5db43421561ed8cea3b17a2dcb210c81fa105a9b
0zoo <e0zoo94@gmail.com>
2019-07-16 12:06:28 +0900

    seperated api url

--- src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
+++ src/main/java/ajou/realcoding/team3/riotGamesPerformances/api/RiotGamesPerformancesAPIClient.java
@@ -10,20 +10,22 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class RiotGamesPerformancesAPIClient {
 
-    private final String apikey = "RGAPI-b241cfe9-a7f8-4382-bf1d-c63b6036c33b";
-    private final String summonorV4Url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={apikey}";
-    private final String leagueV4Url = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedSummonerId}?api_key={apikey}";
+    private final String apikey = "RGAPI-81174976-ddc1-486f-8b4a-5a6ff36ed103";
+    private final String baseUrl = "https://kr.api.riotgames.com";
+    private final String authUrl = "?api_key={apikey}";
+    private final String summonerV4Url = "/lol/summoner/v4/summoners/by-name/{summonerName}";
+    private final String leagueV4Url = "/lol/league/v4/entries/by-summoner/{encryptedSummonerId}";
 
 
     @Autowired
     RestTemplate restTemplate;
 
     public PlayerEncryptedID requestEncryptedID(String summonerName) {
-        return restTemplate.exchange(summonorV4Url, HttpMethod.GET, null, PlayerEncryptedID.class, summonerName, apikey).getBody();
+        return restTemplate.exchange(baseUrl+summonerV4Url+authUrl, HttpMethod.GET, null, PlayerEncryptedID.class, summonerName, apikey).getBody();
     }
 
     public PlayerPerformance[] requestPerformance(String encryptedSummonerId) {
-        PlayerPerformance[] performances = restTemplate.exchange(leagueV4Url, HttpMethod.GET, null, PlayerPerformance[].class, encryptedSummonerId, apikey).getBody();
+        PlayerPerformance[] performances = restTemplate.exchange(baseUrl+leagueV4Url+authUrl, HttpMethod.GET, null, PlayerPerformance[].class, encryptedSummonerId, apikey).getBody();
         return performances;
     }
 
